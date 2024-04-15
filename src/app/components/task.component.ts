599ae7c015772d108f4272d08b444e7520590261
cadolphe <chrisotpher.adolphe@gmail.com>
2024-04-15 12:21:15 +0400

    add stories for composite component

--- src/app/components/task.component.ts
+++ src/app/components/task.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 
-import { Task } from '../models/task.type';
+import { Task } from '../models/task.model';
 
 @Component({
   selector: 'app-task',
