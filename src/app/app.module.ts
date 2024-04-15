0fb469f484af7c8426a5c0277e86dad384b486b5
cadolphe <chrisotpher.adolphe@gmail.com>
2024-04-15 13:02:46 +0400

    add data to container component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { TaskModule } from './components/task.module';
+import { NgxsModule } from '@ngxs/store';
+import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
+import { NgxsLoggerPluginModule } from '@ngxs/logger-plugin';
+
+import { environment } from '../environments/environment';
+
 import { AppComponent } from './app.component';
-import TaskComponent from './components/task.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TaskComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    TaskModule,
+    NgxsModule.forRoot([], {
+      developmentMode: !environment.production,
+    }),
+    NgxsReduxDevtoolsPluginModule.forRoot(),
+    NgxsLoggerPluginModule.forRoot({
+      disabled: environment.production,
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
